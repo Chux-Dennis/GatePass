33831bb3d47bd44f53fa6d7adf7304fd34f199e6
Chux-Dennis <chuksabosi63@gmail.com>
2026-01-05 17:44:29 +0100

    Added controllers for buyers

--- src/routes/Auth.routes.ts
+++ src/routes/Auth.routes.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
 import { newOrganizer,verifyOrganizersOTP as VOOTP ,requestOrganizerOTP as ROOTP,loginOrganizer} from "../controllers/Auth.controllers";
-
+import { newBuyer,loginBuyer,requestBuyerOTP,verifyBuyersOTP } from "../controllers/Auth.controllers";
 const Auth = Router()
 
+// === ORGANIZER ROUTES === 
 Auth.post("/organizer/new",newOrganizer)
 Auth.post("/organizer/verify",VOOTP)
 Auth.post("/organizer/request-otp",ROOTP)
 Auth.post("/organizer/login",loginOrganizer)
+
+
+// === BUYER ROUTES === 
+Auth.post("/buyer/new",newBuyer)
+Auth.post("/buyer/verify",verifyBuyersOTP)
+Auth.post("/buyer/request-otp",requestBuyerOTP)
+Auth.post("/buyer/login",loginBuyer)
 
 
 
--- src/validations/Auth.validate.ts
+++ src/validations/Auth.validate.ts
@@ -8,6 +8,15 @@ export const validateNewOrganizer = Joi.object<any>({
   confirmPassword:Joi.string().min(8).max(50).pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])")).required()
 }).min(1).required()
 
+export const validateNewBuyer = Joi.object<any>({
+  name:Joi.string().max(50).required(),
+  firstname:Joi.string().max(50).required(),
+  lastname:Joi.string().max(50).required(),
+  email:Joi.string().email().required(),
+  password:Joi.string().min(8).max(50).pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])")).required(),
+  confirmPassword:Joi.string().min(8).max(50).pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])")).required()
+}).min(1).required()
+
 export const validateVerifyOTP = Joi.object({
     email: Joi.string().email().required(),
     otp:Joi.string().required(),
